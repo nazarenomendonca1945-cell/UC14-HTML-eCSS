151b4c74e9da5d637a17a095ee36d22d609c9445
nazarenomendonca1945-cell <nazarenomendonca1945@gmail.com>
2026-08-20 17:53:09 -0300

    Add files via upload

---
+++ atividade_ternario/script.js
@@ -0,0 +1,12 @@
+let temperatura = 35;
+let descricao = temperatura > 30 ? 'Quente' : 'Agradável';
+
+let numero = 8;
+let tipo = numero % 2 === 0 ? 'par' : 'ímpar';
+
+let hora = 10;
+let saudacao = hora < 12 ? 'Bom dia' : 'Boa tarde/noite';
+
+console.log(descricao);
+console.log(tipo);
+console.log(saudacao);
